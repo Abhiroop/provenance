15ab7faa1ec2d49c62aa84be64a5a055ee1653f2
Abhiroop Sarkar <asiamgenius@gmail.com>
2018-02-10 14:49:25 +0000

    Brute forced TSP

---
+++ app/TSP.hs
@@ -0,0 +1,38 @@
+{-# LANGUAGE FlexibleContexts#-}
+module TSP where
+
+import DArray
+import Control.Monad(forM_)
+
+import Data.List
+
+ins x [] = [[x]]
+ins x (y:ys) = [ [x,y]++ys ] ++ map (y:) (ins x ys)
+
+perm :: [Int] -> [[Int]]
+perm  [] = [[]]
+perm xs = do
+  a <- xs
+  let m = delete a xs
+  ls <- perm m
+  return (a : ls)
+
+type Matrix = [[Int]]
+
+type Town = Int
+
+type Tour = [Town]
+
+cost :: Matrix -> Tour -> Int
+cost dist (x:xs) = go dist x (x:xs)
+  where go d t [y] = d !! y !! t
+        go d t (x:y:xs) = d !! x !! y + go d t (y:xs)
+
+cheapP :: Matrix -> Tour -> Tour -> Ordering
+cheapP m t1 t2 = compare (cost m t1) (cost m t2)
+
+cheapest :: Matrix -> [Tour] -> Tour
+cheapest m ts = head $ sortBy (\t1 t2 -> cheapP m t1 t2) ts
+
+tsp :: Matrix -> [Int] -> Tour
+tsp m xs = cheapest m $ perm xs
